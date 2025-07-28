bc07a26d1939787cc435ef63b52bf82eb8d778b3
Tommyle38 <lehoangminh9938@gmail.com>
2025-07-28 21:41:43 +1000

    update

--- src/auth/SignInPage.vue
+++ src/auth/SignInPage.vue
@@ -2,8 +2,6 @@
 import { SignIn } from "@clerk/vue";
 import "../style.css";
 
-const baseUrl = import.meta.env.VITE_CLERK_REDIRECT_URL || 'http://localhost:5173';
-const signUpUrl = `${baseUrl}/sign-up`;
 </script>
 
 <template>
@@ -16,8 +14,8 @@ const signUpUrl = `${baseUrl}/sign-up`;
         <h3>PumpMaster</h3>
       </div>
       <div class="flex justify-center items-center pt-[10%]">
-        <SignIn :routing="'path'" :path="'/sign-in'" :sign-up-fallback-redirect-url="signUpUrl" :sign-up-url="signUpUrl"
-          :sign-up-force-redirect-url="signUpUrl" />
+        <SignIn :routing="'path'" :path="'/sign-in'" :sign-up-fallback-redirect-url="'/sign-up'"
+          :sign-up-url="'/sign-up'" :sign-up-force-redirect-url="'/sign-up'" />
       </div>
     </div>
   </main>
